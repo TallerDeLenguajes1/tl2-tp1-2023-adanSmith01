81e088ce9e43a97d4ac5751e0c6f6ebe9369f3ae
adanSmith01 <smith.paredes.13@gmail.com>
2023-08-28 19:40:05 -0300

    Avances del sistema de gestión de pedidos

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using EmpresaDeCadeteria;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string rutaArchivoDatosCadeteria = "datosCadeteria.csv";
+        string rutaArchivoDatosCadetes = "datosCadetes.csv";
+
+        Cadeteria c = AccesoDatosCadeteria.ObtenerInfoCadeteria(rutaArchivoDatosCadeteria); //Verificado
+        c.ListaCadetes = AccesoDatosCadeteria.ObtenerListaCadetes(rutaArchivoDatosCadetes); //Verificado
+
+        //SISTEMA DE GESTIÓN
+        Pedido ped = c.DarAltaPedido();
+        c.AgregarPedido(ped);
+        c.MostrarPedidos();
+    }
+}
